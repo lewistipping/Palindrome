5ef116fa95c712b8b71fb653f4f98c7f7a92b30b
Lewis Tipping <lewistipping01@hotmail.co.uk>
2018-09-10 18:34:08 +0100

    First Commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,38 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+
+
+                Console.WriteLine("Enter a word/phrase to check if it is a Palindrome." +
+                                    "\n Type \"Quit\" to exit)");
+                String input = Console.ReadLine();
+
+                if(input.ToLower() == "quit" )
+                {
+                    break;
+                }
+                else
+                {
+                    char[] charArr = input.ToCharArray();
+                    String word = new String(charArr);
+                    String reverseWord ="";
+                    for(int i = charArr.Length -1;i > -1 ;i--)
+                    {
+                        
+                        reverseWord += charArr[i];
+                    }
+                    if(word.Replace(" ","") == reverseWord.Replace(" ", ""))
+                    {
+                        Console.WriteLine("It's a Palindrome");
+                    }
+                    
+                }
+                
+
+            }
+            
         }
     }
 }
